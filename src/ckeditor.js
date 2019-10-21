38e5d13b4899002099be12598ab23803f694be71
Jonathan Whitney <jonathan.whitney@salesforce.com>
2019-10-21 13:20:05 -0400

    Soldes customizations

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -5,15 +5,13 @@
 
 // The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
@@ -21,26 +19,26 @@ import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import MediaEmbed from '@soldes/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
+	Alignment,
 	Autoformat,
 	Bold,
+	Code,
 	Italic,
-	BlockQuote,
-	CKFinder,
-	EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
@@ -48,13 +46,15 @@ ClassicEditor.builtinPlugins = [
 	ImageToolbar,
 	ImageUpload,
 	Indent,
+	IndentBlock,
 	Link,
 	List,
 	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	Underline
 ];
 
 // Editor configuration.
@@ -62,30 +62,87 @@ ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
 			'heading',
-			'|',
 			'bold',
 			'italic',
+			'underline',
 			'link',
+			'code',
+			'alignment',
+			'|',
 			'bulletedList',
 			'numberedList',
-			'|',
 			'indent',
 			'outdent',
 			'|',
 			'imageUpload',
-			'blockQuote',
-			'insertTable',
 			'mediaEmbed',
+			'insertTable',
+			'|',
 			'undo',
 			'redo'
 		]
 	},
+	heading: {
+		options: [
+			{
+				model: 'sldsHeadingLarge',
+				view: {
+					name: 'div',
+					classes: 'slds-text-heading_large'
+				},
+				title: 'Heading Large',
+				class: 'ck-heading_slds-text-heading-large',
+				converterPriority: 'high'
+			},
+			{
+				model: 'sldsHeadingMedium',
+				view: {
+					name: 'div',
+					classes: 'slds-text-heading_medium'
+				},
+				title: 'Heading Medium',
+				class: 'ck-heading_slds-text-heading-medium',
+				converterPriority: 'high'
+			},
+			{
+				model: 'sldsHeadingSmall',
+				view: {
+					name: 'div',
+					classes: 'slds-text-heading_small'
+				},
+				title: 'Heading Small',
+				class: 'ck-heading_slds-text-heading-small',
+				converterPriority: 'high'
+			},
+			{
+				model: 'paragraph',
+				title: 'Paragraph',
+				class: 'ck-heading_paragraph'
+			},
+			{
+				model: 'sldsTextBodySmall',
+				view: {
+					name: 'p',
+					classes: 'slds-text-body_small'
+				},
+				title: 'Paragraph Small',
+				class: 'ck-heading_slds-text-body-small',
+				converterPriority: 'high'
+			}
+		]
+	},
 	image: {
 		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
+			'imageTextAlternative',
 			'|',
-			'imageTextAlternative'
+			'imageStyle:alignLeft',
+			'imageStyle:full',
+			'imageStyle:alignRight'
+		],
+		styles: [
+			'full',
+			'alignLeft',
+			'alignRight'
 		]
 	},
 	table: {
@@ -94,6 +151,9 @@ ClassicEditor.defaultConfig = {
 			'tableRow',
 			'mergeTableCells'
 		]
+	},
+	mediaEmbed: {
+		previewsInData: true
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,87 @@ module.exports = {
 		new webpack.BannerPlugin( {
 			banner: bundler.getLicenseBanner(),
 			raw: true
-		} )
+		} ),
+		new webpack.NormalModuleReplacementPlugin(
+			/pilcrow\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/edit_form.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/bold\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/bold.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/italic\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/italic.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/underline\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/underline.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/link\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/link.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/unlink\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/remove_link.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/code\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/insert_tag_field.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/align-left\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/left_align_text.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/align-center\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/center_align_text.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/align-right\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/right_align_text.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/align-justify\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/justify_text.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/bulletedlist\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/richtextbulletedlist.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/numberedlist\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/richtextnumberedlist.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/indent\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/richtextindent.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/outdent\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/richtextoutdent.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/image\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/image.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/media\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/video.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/table\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/table.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/undo\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/undo.svg'
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/redo\.svg/,
+			path.resolve( __dirname, 'theme' ) + '/icons/utility/redo.svg'
+		),
 	],
 
 	module: {
